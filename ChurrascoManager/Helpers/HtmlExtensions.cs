d1018ff262ce521975563c8dfc02c5c2d99eb3eb
Fabian <fabian@correamarques.com.br>
2017-12-20 19:19:09 -0200

    disable dropdownlist if

---
+++ ChurrascoManager/Helpers/HtmlExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace ChurrascoManager.Helpers
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString DropDownList(this HtmlHelper html, string name, IEnumerable<SelectListItem> selectList, object htmlAttributes, bool canEdit )
+        {
+            var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+            if (!canEdit)
+                attributes.Add("disabled", "disabled");
+
+            return html.DropDownList(name, selectList, attributes);
+        }
+
+    }
+}
