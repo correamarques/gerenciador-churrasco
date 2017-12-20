2d1fc5d99aa03833eb622b95d232f02ab0b9175d
Fabian <fabian@correamarques.com.br>
2017-12-20 14:22:18 -0200

    add helpers

---
+++ ChurrascoManager/Helpers/hDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChurrascoManager.Helpers
+{
+    public class hDateTime
+    {
+        public static DateTime GetLastFridayOfTheMonth(DateTime date)
+        {
+            var lastDayOfMonth = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+
+            while (lastDayOfMonth.DayOfWeek != DayOfWeek.Friday)
+                lastDayOfMonth = lastDayOfMonth.AddDays(-1);
+
+            return lastDayOfMonth;
+        }
+    }
+}
