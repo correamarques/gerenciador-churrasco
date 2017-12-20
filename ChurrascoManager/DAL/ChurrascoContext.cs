13e54fc9c507b14b2d60254d602e9e0abe2ee0dd
Fabian <fabian@correamarques.com.br>
2017-12-20 14:21:33 -0200

    add db context

---
+++ ChurrascoManager/DAL/ChurrascoContext.cs
@@ -0,0 +1,21 @@
+﻿using ChurrascoManager.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ChurrascoManager.DAL
+{
+    public class ChurrascoContext : DbContext
+    {
+        public ChurrascoContext() : base("ChurrascoContext") { }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
