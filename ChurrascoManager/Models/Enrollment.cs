ba37c4f848105d28c3155d6abc8c4bfd6454bd1e
Fabian <fabian@correamarques.com.br>
2017-12-20 18:03:24 -0200

    enrollment index and system title

--- ChurrascoManager/Models/Enrollment.cs
+++ ChurrascoManager/Models/Enrollment.cs
@@ -1,13 +1,21 @@
-﻿namespace ChurrascoManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChurrascoManager.Models
 {
     public class Enrollment
     {
         public int ID { get; set; }
+        [Display(Name = "Evento")]
         public int EventID { get; set; }
+        [Display(Name = "Nome")]
         public int PersonID { get; set; }
+        [Display(Name = "Pago")]
         public bool Paid { get; set; }
+        [Display(Name = "Vai beber?")]
         public bool Drink { get; set; }
+        [Display(Name = "Valor pago")]
         public decimal Amount { get; set; }
+        [Display(Name = "Observação")]
         public string Observation { get; set; }
 
         public virtual Person Person { get; set; }
