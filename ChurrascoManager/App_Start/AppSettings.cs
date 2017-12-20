fc9c2f44722cffb1a79c3574406a22cadd3ec4d0
Fabian <fabian@correamarques.com.br>
2017-12-20 17:42:09 -0200

    appsettings name of system

---
+++ ChurrascoManager/App_Start/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.Web.Configuration;
+
+namespace ChurrascoManager
+{
+    public class AppSettings
+    {
+        public static string SystemTitle { get { return Setting<string>("system:Title"); } }
+
+
+        private static T Setting<T>(string name)
+        {
+            string value = ConfigurationManager.AppSettings[name];
+
+            if (value == null)
+                value = WebConfigurationManager.AppSettings[name];
+
+            if (value == null)
+                throw new Exception(String.Format("Could not find setting '{0}',", name));
+
+            return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
+        }
+    }
+}
