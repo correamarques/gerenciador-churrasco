9444552e1eb1298a609a86844472d153681be90d
Fabian <fabian@correamarques.com.br>
2017-12-20 14:36:26 -0200

    Models DataAnnotation

--- ChurrascoManager/Models/Event.cs
+++ ChurrascoManager/Models/Event.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChurrascoManager.Models
 {
     public class Event
     {
         public int ID { get; set; }
+        [Display(Name = "Título")]
         public string Title { get; set; }
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
+        [Display(Name = "Data"), DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
--- ChurrascoManager/Models/Person.cs
+++ ChurrascoManager/Models/Person.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChurrascoManager.Models
 {
     public class Person
     {
         public int ID { get; set; }
+        [Display(Name = "Nome")]
         public string Name { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
