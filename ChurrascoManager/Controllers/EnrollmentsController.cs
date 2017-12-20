9dd4a931a46d36d7010851d3134ff642319e3881
Fabian <fabian@correamarques.com.br>
2017-12-20 17:55:13 -0200

    controller enrollments

---
+++ ChurrascoManager/Controllers/EnrollmentsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChurrascoManager.DAL;
+using ChurrascoManager.Models;
+
+namespace ChurrascoManager.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private ChurrascoContext db = new ChurrascoContext();
+
+        // GET: Enrollments
+        public ActionResult Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.Event).Include(e => e.Person);
+            return View(enrollments.ToList());
+        }
+
+        // GET: Enrollments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventID = new SelectList(db.Events, "ID", "Description");
+            ViewBag.PersonID = new SelectList(db.Persons, "ID", "Name");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,EventID,PersonID,Paid,Drink,Amount,Observation")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EventID = new SelectList(db.Events, "ID", "Description", enrollment.EventID);
+            ViewBag.PersonID = new SelectList(db.Persons, "ID", "Name", enrollment.PersonID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventID = new SelectList(db.Events, "ID", "Description", enrollment.EventID);
+            ViewBag.PersonID = new SelectList(db.Persons, "ID", "Name", enrollment.PersonID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,EventID,PersonID,Paid,Drink,Amount,Observation")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventID = new SelectList(db.Events, "ID", "Description", enrollment.EventID);
+            ViewBag.PersonID = new SelectList(db.Persons, "ID", "Name", enrollment.PersonID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
